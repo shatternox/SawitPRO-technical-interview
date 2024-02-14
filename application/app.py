4d9007bff5fcaf038782776e47910060b42cc6c1
danny-insignia <126647144+danny-insignia@users.noreply.github.com>
2024-02-15 01:00:03 +0700

    db fix, docker fix, upload features, display, etc.

--- application/app.py
+++ application/app.py
@@ -1,36 +1,34 @@
-from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash, session, abort
+from flask import (
+    Flask, render_template, request, redirect, url_for, flash, session
+)
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from functools import wraps
 from werkzeug.utils import secure_filename
-import MySQLdb.cursors
-import os
-import re
-import secrets
+from flask_cors import CORS
+from SecureEncrypt import AESCipher
+import MySQLdb.cursors, os, re, secrets, base64
 
 app = Flask(__name__)
-
+CORS(app)
 bcrypt = Bcrypt(app)
 
-app.secret_key = '4528f440-1791-4770-b153-73dae6de7f73'
+# Configure app secret key and MySQL connection
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
-app.config['MYSQL_HOST'] = 'db'  # Docker service name for MySQL container
-app.config['MYSQL_USER'] = 'sawidproadmin'
-app.config['MYSQL_PASSWORD'] = 'vaeiiGbL%j3jjQ!6duFKk79koKYLch%d'
-app.config['MYSQL_DB'] = 'sawitpro'
-
+# Initialize MySQL
 mysql = MySQL(app)
 
-# Create Blueprint for authentication
-auth = Blueprint('auth', __name__)
-
-# User Registration & Login routes
-
-# Image Upload, Storage, Retrieval, and Deletion
+# Configure upload folder and allowed file extensions
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+# Authorization decorator
 def authorize(f):
     @wraps(f)
     def decorated_function(*args, **kws):
@@ -39,146 +37,138 @@ def authorize(f):
         return f(*args, **kws)
     return decorated_function
 
+# Function to check allowed file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+# Register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    message = ""
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'confirm_password' in request.form and 'email' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-        email = request.form['email']
+    if request.method == 'POST':
+        # Handle form submission
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        email = request.form.get('email')
 
+        # Validate form inputs
+        if not (username and password and email):
+            flash('Please fill out the form!', 'alert alert-danger')
+            return redirect(url_for('register'))
+        if password != confirm_password:
+            flash('Passwords do not match!', 'alert alert-danger')
+            return redirect(url_for('register'))
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Invalid email address!', 'alert alert-danger')
+            return redirect(url_for('register'))
+
+        # Check if user already exists
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM users WHERE email = %s OR username = %s', (email, username))
         account = cursor.fetchone()
         if account:
-            message = 'Account already exists!'
-            flash(f'{message}', 'alert alert-danger')
+            flash('Account already exists!', 'alert alert-danger')
             return redirect(url_for('register'))
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            message = 'Invalid email address!'
-            flash(f'{message}', 'alert alert-danger')
-            return redirect(url_for('register'))
-        elif not username or not password or not email:
-            message = 'Please fill out the form!'
-            flash(f'{message}', 'alert alert-danger')
-            return redirect(url_for('register'))
-        elif password != confirm_password:
-            message = 'Passwords do not match!'
-            flash(f'{message}', 'alert alert-danger')
-            return redirect(url_for('register'))
-        else:
-            image_key = secrets.token_hex(16)
-            hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
-            cursor.execute('INSERT INTO users VALUES (NULL, %s, %s, %s, %s)', (username, hash_password, email, image_key))
-            mysql.connection.commit()
-            message = 'You have successfully registered!'
-            flash(f'{message}', 'alert alert-success')
-            return redirect(url_for('login'))
 
-    elif request.method == 'POST':
-        message = 'Please fill out the form!'
-        flash(f'{message}', 'alert alert-danger')
+        # Register user
+        image_key = secrets.token_hex(16)
+        hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        cursor.execute('INSERT INTO users VALUES (NULL, %s, %s, %s, %s)', (username, hash_password, email, image_key))
+        mysql.connection.commit()
+        flash('You have successfully registered!', 'alert alert-success')
+        return redirect(url_for('login'))
 
     return render_template('register.html')
 
-
+# Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        email = request.form['email']
-        password = request.form['password']
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
         data = cursor.fetchone()
 
-        if data:
-            is_valid = bcrypt.check_password_hash(data['password'], password)
-        
-            if is_valid:
-                session['loggedin'] = True
-                session['id'] = data['user_id']
-                session['username'] = data['username']
-                session['email'] = data['email']
-                message = "Logged in successfully!"
-                flash(f'{message}', 'alert alert-success')
-                return redirect(url_for('home'))
-            else:
-                message = 'Incorrect email or password!'
-                flash(f'{message}', 'alert alert-danger')
-        else:
-            message = 'Incorrect email or password!'
-            flash(f'{message}', 'alert alert-danger')
+        if data and bcrypt.check_password_hash(data['password'], password):
+            session['loggedin'] = True
+            session['id'] = data['user_id']
+            session['username'] = data['username']
+            session['email'] = data['email']
+            session['image_key'] = data['image_key']
+            flash('Logged in successfully!', 'alert alert-success')
+            return redirect(url_for('home'))
+        flash('Incorrect email or password!', 'alert alert-danger')
 
     return render_template('login.html')
 
-
+# Logout route
 @app.route('/logout')
 def logout():
-
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    session.pop('email', None)
-
+    session.clear()
     return redirect(url_for('login'))
 
-
+# Home route
 @app.route('/', methods=['GET'])
 @authorize
 def home():
-    return render_template('index.html', username=session['username'])
-        
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM images WHERE user_id = %s', (session['id'],))
+    images = cursor.fetchall() or []
 
+    decrypted_images = []
+    for image in images:
+        cipher = AESCipher(session['image_key'])
+        decrypted_image = cipher.decrypt(image['image_data'])
+        decrypted_images.append({
+            "image_data": decrypted_image,
+            "image_name": image['filename'],
+            "extension": image['filename'].split('.')[-1]
+        })
+
+    return render_template('index.html', username=session['username'], images=decrypted_images)
+
+# Gallery route
+@app.route('/gallery', methods=['GET'])
+@authorize
+def gallery():
+    return render_template('gallery.html', username=session['username'])
+
+# Upload route
 @app.route('/upload', methods=['POST'])
+@authorize
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part', 'error')
+            flash('Please select an image to upload', 'alert alert-danger')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file', 'error')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File uploaded successfully', 'success')
-            # Save the filename into the database or perform other actions
-            return redirect(url_for('home'))
-        else:
-            flash('File type not allowed', 'error')
+        if file.filename == '' or not allowed_file(file.filename):
+            flash('Please select a proper image', 'alert alert-danger')
             return redirect(request.url)
 
+        filename = secure_filename(file.filename)
+        image_base64 = base64.b64encode(file.read()).decode('utf-8')
+        cipher = AESCipher(session['image_key'])
+        encrypted_image = cipher.encrypt(image_base64)
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO images VALUES (NULL, %s, %s, %s)', (session['id'], filename, encrypted_image))
+        mysql.connection.commit()
+        flash('File uploaded successfully', 'success')
+        return redirect(url_for('home'))
+
+    flash('File type not allowed', 'alert alert-danger')
+    return redirect(url_for('home'))
+
+# Delete file route
 @app.route('/delete/<filename>', methods=['POST'])
+@authorize
 def delete_file(filename):
-    # Delete file from storage
     os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # Perform necessary actions in the database to delete the filename entry
     flash('File deleted successfully', 'success')
     return redirect(url_for('home'))
 
-# User Feedback
-@app.route('/feedback', methods=['POST'])
-def feedback():
-    if request.method == 'POST':
-        # Get form data
-        feedback = request.form['feedback']
-        # Insert feedback into database or perform necessary actions
-        flash('Thank you for your feedback!', 'success')
-    return redirect(url_for('home'))
-
 if __name__ == '__main__':
-    app.register_blueprint(auth)
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.run(host='0.0.0.0', port=6969, debug=True)
-
-
